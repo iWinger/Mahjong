dd79a7918d14fb3d339c969b43c43444b77af022
Wing <chanwingjun3@gmail.com>
2018-12-24 04:58:09 -0500

    htmlfile

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,7 @@ var http = require('http');
 
 var server = http.createServer(function(req,res){
 	res.writeHead(200, {"Content-Type": "text/html"});
+	res.end('Hello <strong>world!</strong><br/> From Mahjong Company!');
 	res.end();
 });
 
